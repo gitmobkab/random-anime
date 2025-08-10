1ab2284610b60fa497470ae4e69191ad481663bc
gitmobkab <richmondaurel77@gmail.com>
2025-08-10 13:52:24 +0000

    add data_unwrapper function to extract nested attributes from data

--- random_anime.py
+++ random_anime.py
@@ -9,3 +9,13 @@ def make_request(api_url:str) -> dict | None:
     else:
         data = request.json()
         return data
+    
+def data_unwrapper(data:dict, list_of_attributes: list[str]):
+    target_attribute = data[list_of_attributes[0]]
+    try:
+        for attribute in list_of_attributes[1:]:
+            target_attribute = target_attribute[attribute]
+    except IndexError:
+        print("Error: The list should at least contain 2 attributes")
+        return
+    
