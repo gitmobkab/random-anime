020c12dd4f3a8c27593a1f9c6bf0327ec276007d
gitmobkab <richmondaurel77@gmail.com>
2025-08-12 00:18:08 +0000

    add launch.sh script for Python environment setup and package installation

---
+++ launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! command -v python &> /dev/null
+then
+    echo "it looks like python is not installed."
+    echo "if you think it's an error, try executing the file manually with 'python random_anime.py'"
+    echo "if indeed python is not installed please install the latest version on the official website or update python (>=3.7)"
+    exit 1
+fi
+
+pip install requests flask pillow
